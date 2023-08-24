68454e3a346523cfa78253d918636cd54f5ca060
WellingtonLFaria <wellingtonll.faria@gmail.com>
2023-08-23 23:25:03 -0300

    Primeiros 3 exercícios

---
+++ exercicio 1/index.ts
@@ -0,0 +1,74 @@
+// Classe para a criação dos campos de entrada do usuário
+class campoEntradaUsuario {
+    nome:string;
+    tipo:string;
+    parente:HTMLElement;
+    label:HTMLLabelElement;
+    campo:HTMLInputElement;
+    constructor(nome:string, tipo:string, parente:HTMLElement) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.parente = parente;
+
+        this.label = document.createElement("label");
+        this.label.innerText = `${nome}:`
+
+        this.campo = document.createElement("input");
+        this.campo.type = tipo;
+
+        this.parente.appendChild(this.label);
+        this.parente.appendChild(this.campo);
+    }
+}
+
+// Função que calcula o IMC e a classficação
+function IMC(altura:number, peso:number) {
+    let msg;
+    let imc = peso/(altura/100)**2;
+    if ( imc < 16) {
+        msg = "Baixo peso muito grave";
+    } else if ( imc >= 16 && imc <= 16.99) {
+        msg = "Baixo peso grave";
+    } else if ( imc >= 17 && imc <= 18.49) {
+        msg = "Baixo peso";
+    } else if ( imc >= 18.50 && imc <= 24.99) {
+        msg = "Peso normal";
+    } else if ( imc >= 25 && imc <= 29.99) {
+        msg = "Sobrepeso";
+    } else if ( imc >= 30 && imc <= 34.99) {
+        msg = "Obesidade grau I";
+    } else if ( imc >= 35 && imc <= 39.99) {
+        msg = "Obesidade grau II";
+    } else if ( imc >= 40) {
+        msg = "Obesidade grau III";
+    }
+    return [Math.round(peso/(altura/100)**2), msg];
+}
+
+// Criando a área do formulário
+const form = document.createElement("div");
+document.body.appendChild(form);
+
+// Criando os campos de entrada do usuário
+const campoNome = new campoEntradaUsuario("Nome", "text", form);
+const campoAltura = new campoEntradaUsuario("Altura(cm)", "number", form);
+const campoPeso = new campoEntradaUsuario("Peso(kg)", "number", form);
+
+// Criando o botão de envio
+const button = document.createElement("button");
+button.innerText = "Enviar";
+form.appendChild(button);
+
+// Criando o parágrafo do resultado do cáculo
+const res = document.createElement("p");
+form.appendChild(res);
+
+// Quando clica no botão de enviar
+button.onclick = () => {
+    let nome = campoNome.campo.value;
+    let altura = Number(campoAltura.campo.value);
+    let peso = Number(campoPeso.campo.value);
+    let imc = IMC(altura, peso);
+    
+    res.innerText = `IMC: ${String(imc[0])}, classificado como: ${imc[1]}`;
+};
---
+++ exercicio 3/index.js
@@ -0,0 +1,58 @@
+"use strict";
+const peso1 = 2;
+const peso2 = 5;
+const peso3 = 3;
+function calcularMedia(nota1, nota2, nota3) {
+    let media = ((nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3)) / (peso1 + peso2 + peso3);
+    let classificacao;
+    if (media > 9 && media <= 10) {
+        classificacao = "A";
+    }
+    else if (media > 8 && media <= 9) {
+        classificacao = "B";
+    }
+    else if (media > 7 && media <= 8) {
+        classificacao = "C";
+    }
+    else if (media > 6 && media <= 7) {
+        classificacao = "D";
+    }
+    else if (media > 5 && media <= 6) {
+        classificacao = "E";
+    }
+    else if (media >= 0 && media <= 5) {
+        classificacao = "F";
+    }
+    return [media, classificacao];
+}
+;
+const labelAtividade1 = document.createElement("label");
+labelAtividade1.innerText = "Nota atividade 1:";
+const campoAtividade1 = document.createElement("input");
+campoAtividade1.type = "number";
+const labelAtividade2 = document.createElement("label");
+labelAtividade2.innerText = "Nota atividade 2:";
+const campoAtividade2 = document.createElement("input");
+campoAtividade2.type = "number";
+const labelAtividade3 = document.createElement("label");
+labelAtividade3.innerText = "Nota atividade 3:";
+const campoAtividade3 = document.createElement("input");
+campoAtividade3.type = "number";
+const submit = document.createElement("button");
+submit.innerText = "Enviar";
+const res = document.createElement("p");
+document.body.appendChild(labelAtividade1);
+document.body.appendChild(campoAtividade1);
+document.body.appendChild(labelAtividade2);
+document.body.appendChild(campoAtividade2);
+document.body.appendChild(labelAtividade3);
+document.body.appendChild(campoAtividade3);
+document.body.appendChild(submit);
+document.body.appendChild(res);
+submit.onclick = () => {
+    let nota1 = Number(campoAtividade1.value);
+    let nota2 = Number(campoAtividade2.value);
+    let nota3 = Number(campoAtividade3.value);
+    let media = calcularMedia(nota1, nota2, nota3);
+    res.innerText = `Média: ${media[0]} | Classificação: ${media[1]}`;
+};
